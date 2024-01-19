3958c03210a3c76fb1d0f225e0e89c8c1932ab9b
komiifo <nathan440@live.fr>
2024-01-19 15:28:04 +0400

    Update FollowingsModal.jsx

--- src/components/modals/FollowingsModal.jsx
+++ src/components/modals/FollowingsModal.jsx
@@ -41,7 +41,7 @@ const FollowingsModal = () => {
             <div className="modal-dialog modal-dialog-centered">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h1 className="modal-title fs-5">Followers</h1>
+                        <h1 className="modal-title fs-5">Suivi(e)s</h1>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={closeModal}></button>
                     </div>
                     <div className="modal-body">
