123356e5adbfe34148f4d45de95d0fe3851cd234
komiifo <nathan440@live.fr>
2024-01-22 09:45:14 +0400

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import Posts from '../components/Posts';
 import User from '../components/Users';
 import Navbar from '../components/modals/Navbar';
+import { useUser } from '../contexts/UserContext';
 
 const Home = () => {
+    const { userProfile } = useUser();
 
     return (
         <>
             <div className="container">
+
+
                 <Navbar />
                 <User />
                 <Posts />
+
             </div>
         </>
     )
